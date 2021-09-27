30e474705b276fea5bd90998477e65ec5b5f3d7c
Samvw90 <samuel.villegas90@gmail.com>
2021-09-27 13:59:36 -0700

    Initial client bolierplate

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to Coffee Connect by Javalimos the best team ever</h1>
+        </div>
+    )
+}
+
+export default App
