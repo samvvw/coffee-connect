094de4db823a783cc4733033cce2ac8e995a6fda
Megumi Takashima <mtakashima00@mylangara.ca>
2021-10-05 17:26:16 -0700

    Created userSchema and productSchema

---
+++ server/src/models/product.model.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose')
+// const validator = require('validator')
+
+
+const mediaSchema = new mongoose.Schema(
+    {
+        mediaName: {
+            type: String,
+        },
+
+        mediaUrl: {
+            type: String,
+        },
+
+        mediaType: {
+            type: String,
+        },
+    }
+)
+
+
+const productVariationSchema = new mongoose.Schema(
+    {
+        price: {
+            type: Number,
+            min: 0,
+        },
+
+        weight: {
+            type: Number,
+            min: 0,
+        },
+
+        unit: {
+            type: String,
+            enum: {
+                values: ['g', 'kg', 'lb', 'oz'],
+                message: '{VALUE} is not supported',
+            },
+        },
+    }
+)
+
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please input your farm name'],
+            minLength: 2,
+            maxLength: 15,
+        },
+
+        description: {
+            type: String,
+            required: [true, 'Please input description of your farm'],
+            maxLength: 1000,
+        },
+
+        farmId: {
+            type: String,
+            required: [true, 'Missing farm ID'],
+        },
+
+        media: [mediaSchema],
+
+        variety: {
+            type: String,
+        },
+
+        coffeeProcess: {
+            type: String,
+        },
+
+        flavorNotes: {
+            type: [String],
+        },
+
+        roastDate: {
+            type: Date,
+        },
+
+        productVariations: [productVariationSchema],
+
+        reviews: {
+            type: [String],
+        },
+
+        score: {
+            type: Number,
+        },
+
+        aroma: {
+            type: String,
+        },
+    },
+
+    {
+        timestamps: true,
+    }
+)
---
+++ server/src/models/user.model.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, 'Please input your first name'],
+            minLength: 2,
+            maxLength: 15,
+        },
+
+        lastName: {
+            type: String,
+            required: [true, 'Please input your last name'],
+            minLength: 2,
+            maxLength: 15,
+        },
+
+        email: {
+            type: String,
+            required: [true, 'Please input your email'],
+            validate: [
+                {
+                    //if the email passed the validation, it will return true
+                    //if the email didn't pass the validation, it will goes to message
+                    validator: function(email){
+                        return validator.isEmail(email)
+
+                    },
+                    messages: `Please input a valid email`
+                },
+                
+                {
+                    //if the count returns 0 (=false), will return !false = true, and it passes the validation
+                    //if the count returns 1 (=true), will return !true = false, and it fails the validation and return message 
+                    validator: async function(email){
+                    const exists = await mongoose.models.User.countDocuments({ email }) 
+                    return !exists 
+                    },
+            
+                    messages: (props) => `${props.value} already exist`
+                }
+            ]
+        },
+
+        userName: {
+            type: String,
+            required: [true, 'Please input your user name'],
+            minLength: 3,
+            maxLength: 15,
+        },
+
+        password: {
+            type: String,
+            required: [true, 'Please input password'],
+            minLength: 8,
+        },
+
+        userType: {
+            type: String,
+            required: [true, 'Please select user type'],
+            enum: {
+                values: ['farmer', 'consumer'],
+                message: '{VALUE} is not supported',
+            },
+        },
+
+        bookmarks: {
+            type: [String],
+        },
+
+        farms: {
+            type: [String],
+        } 
+    },
+
+    {
+    timestamps: true,
+    }
+);
