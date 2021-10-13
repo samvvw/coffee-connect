f6dafa2a15bb4d19143609cb6dc44a03ada68039
Megumi Takashima <mtakashima00@mylangara.ca>
2021-10-12 18:30:38 -0700

    feat create getFarms and getFarmById

--- server/src/controllers/farm.controller.js
+++ server/src/controllers/farm.controller.js
@@ -37,3 +37,29 @@ exports.createFarm = async (req, res) => {
         res.status(500).send(error)
     }
 }
+
+
+exports.getFarms = (req, res) => {
+    //Need to consider how we should work with score(rate)
+    //Also can add more fields to return if needed
+    Farm.find({},{"name":1, "region":1, "country":1, "about":1}).exec()
+    .then(result => {
+        res.json(result);
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).send(error)
+    });
+};
+
+
+exports.getFarmById = (req, res) => {
+    Farm.findOne({'_id': req.params.id}).exec()
+    .then(result => {
+        res.json(result);
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).send(error)
+    });
+};
--- server/src/routes/farm.routes.js
+++ server/src/routes/farm.routes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const farmRouter = express.Router()
-const { createFarm } = require('../controllers/farm.controller')
+const { createFarm, getFarms, getFarmById } = require('../controllers/farm.controller')
 
 // POST Farm - Create a new farm
 farmRouter.post('/', createFarm)
 
 // GET Farms - Gets all farms
+farmRouter.get('/', getFarms)
 
 // GET Farm - Gets a single farm
+farmRouter.get('/:id', getFarmById)
 
 module.exports = farmRouter
