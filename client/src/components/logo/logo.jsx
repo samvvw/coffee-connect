103ea8a285e25506f2a344d623655bbcead9c8d2
Jose Arteaga <jarteaga00@mylangara.ca>
2021-10-14 13:16:28 -0700

    feat: add search bar component

--- client/src/components/logo/logo.jsx
+++ client/src/components/logo/logo.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
 import { StyledLogo } from './logo.styles'
 
 const Logo = (props) => {
-    return <StyledLogo>QAFA</StyledLogo>
+    return (
+        <StyledLogo>
+            <Link to="/">QAFA</Link>
+        </StyledLogo>
+    )
 }
 
 export default Logo
--- client/src/components/logo/logo.styles.js
+++ client/src/components/logo/logo.styles.js
@@ -3,4 +3,9 @@ import styled from 'styled-components'
 export const StyledLogo = styled.h1`
     color: #000000;
     font-size: 2.5rem;
+
+    a {
+        text-decoration: none;
+        color: #000000;
+    }
 `
