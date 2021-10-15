5363d0f1fe03e928876acc141a6387ce9c9a36e3
Alejandra Hernandez <ehernandezvega00@mylangara.ca>
2021-10-14 22:43:48 -0700

    feature/25 Created Whats Qafa component

---
+++ client/src/components/WhatSQafa/whatsQafa.jsx
@@ -0,0 +1,16 @@
+import { Heading, Container, StyledParagraph } from './whatsQaja.styles'
+
+const WhatsQafa = () => {
+    return (
+        <Container>
+            <Heading>What is Qafa</Heading>
+            <StyledParagraph>
+                Mollis bibendum leo, pulvinar blandit gravida nec. Adipiscing
+                sed pellentesque cras massa urna lectus leo, ut. Eget dapibus
+                eros, ullamcorper vestibulum vel congue proin.
+            </StyledParagraph>
+        </Container>
+    )
+}
+
+export default WhatsQafa
