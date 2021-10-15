5bb29bb496681ffdd59a2a1f43269b7d42554d18
Jose Arteaga <jarteaga00@mylangara.ca>
2021-10-14 21:38:39 -0700

    feat: feature/40 add How Qafa Works component

---
+++ client/src/components/howQafaWorks/howQafaWorks.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+`
+
+export const Heading = styled.h2`
+    font-weight: bold;
+    font-size: 2rem;
+    margin-bottom: 0;
+`
+export const StyledParagraph = styled.p`
+    font-size: 1.1rem;
+    width: 55ch;
+`
+
+export const HorizontalContainer = styled.div`
+    display: flex;
+    gap: 1rem;
+`
