b81c760a881cc976cb71fba3a62fad25c09a3411
Alejandra Hernandez <ehernandezvega00@mylangara.ca>
2021-10-19 11:20:09 -0700

    feature/75 created dropdownList component

---
+++ client/src/components/dropddownList/dropdownList.jsx
@@ -0,0 +1,41 @@
+import { SelectContainer } from './dropdownList.styles'
+
+const DropdownList = ({
+    width,
+    height,
+    selectName,
+    selectId,
+    objArrayValues,
+}) => {
+    // objArrayValues = [
+    //     {
+    //         id: '1',
+    //         option: 'Qty 1',
+    //     },
+    //     {
+    //         id: '2',
+    //         option: 'Qty 2',
+    //     },
+    //     {
+    //         id: '3',
+    //         option: 'Qty 3',
+    //     },
+    // ]
+
+    let optionTemplate = objArrayValues.map((option) => (
+        <option value={option.id}>{option.option}</option>
+    ))
+
+    return (
+        <SelectContainer
+            name={selectName}
+            id={selectId}
+            width={width}
+            height={height}
+        >
+            {optionTemplate}
+        </SelectContainer>
+    )
+}
+
+export default DropdownList
