be055cbae641152b366b1bbbeb8c43ac69e1204f
Alejandra Hernandez <ehernandezvega00@mylangara.ca>
2021-10-19 00:20:28 -0700

    feature/57 Clean app

--- client/src/App.js
+++ client/src/App.js
@@ -9,39 +9,10 @@ import SignIn from './views/sign-in'
 import WelcomeUser from './views/welcome-user'
 import Home from './views/home/home'
 
-import SelectedButton from './components/selectedButton/selectedButton'
-// import ConnectWithFarmer from './components/connectWithFarmerCard/connectWithFarmerCard'
-// import placeHolder from './assets/images/video-placeholder.png'
-
-// import CircleButton from './components/circleButton/circleButton'
-
-import ProductDetailHeader from './components/productDetail/header/productDetailHeader'
 function App() {
     return (
         <>
             <Header />
-            {/* <CircleButton IconName="FavoriteEmpty" onClick="" /> */}
-            <ProductDetailHeader
-                productName="Product Name"
-                farmName="Farm Name"
-                origin="South America"
-                location="Colombia"
-                altitude="1200masl"
-            />
-
-            {/* <ConnectWithFarmer
-                farmersName="Jose Villalobos"
-                urlImgProfile={placeHolder}
-            /> */}
-
-            <SelectedButton
-                title="340gr"
-                onClick=""
-                backgroundColor="Yellow"
-                backgroundColorClicked="Blue"
-                textColor="Red"
-                textColorClicked="White"
-            />
             <main>
                 <Switch>
                     <Route path="/sign-up" component={SignUp} />
